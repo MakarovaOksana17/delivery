7b82d60423bf239557e1e8b87c0e52e2085b4709
PC\Oksana <oy.makarova17@gmail.com>
2025-08-12 01:02:27 +0300

    add location

---
+++ DeliveryApp.Core/Domian/Model/SharedKernel/Location.cs
@@ -0,0 +1,56 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Core.Domian.Model.SharedKernel
+{
+    public class Location : ValueObject
+    {
+        private const int MAX_COORDINATE = 10;
+        private const int MIN_COORDINATE = 1;
+
+        public int X { get; private set; } 
+        public int Y { get; private set; }
+
+        [ExcludeFromCodeCoverage]
+        private Location() { }
+
+        private Location(int x, int y): this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Location MaxLocation => new Location(MAX_COORDINATE, MAX_COORDINATE);
+        public static Location MinLocation => new Location(MIN_COORDINATE, MIN_COORDINATE);
+
+        public static Result<Location,Error> Create(int x, int y)
+        {
+            if (x < MinLocation.X || x > MaxLocation.X) return GeneralErrors.ValueIsRequired(nameof(x));
+            if (y < MinLocation.Y || y > MaxLocation.Y) return GeneralErrors.ValueIsRequired(nameof(y));
+            return new Location(x, y);
+        }
+
+        public static Location CreateRandom() =>
+            new Location(new Random().Next(MinLocation.X, MaxLocation.X + 1), new Random().Next(MinLocation.Y, MaxLocation.Y + 1));
+        
+
+        public Result<int,Error> DistanceTo(Location traget)
+        {
+            if (traget == null) return GeneralErrors.ValueIsRequired(nameof(traget));
+            return Math.Abs(X - traget.X) + Math.Abs(Y - traget.Y);
+        }
+
+        [ExcludeFromCodeCoverage]
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return X;
+        }
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domian/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,82 @@
+﻿using DeliveryApp.Core.Domian.Model.SharedKernel;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domian.Model.SharedKernel
+{
+    public class LocationShould
+    {
+        [Theory]
+        [InlineData(1,1)]
+        [InlineData(2,9)]
+        public void BeCorrectWhenParamsAreCorrectOnCreated(int x, int y)
+        {
+            var location = Location.Create(x, y);
+            location.IsSuccess.Should().BeTrue();
+            location.Value.X.Should().Be(x);
+            location.Value.Y.Should().Be(y);
+        }
+
+        [Theory]
+        [InlineData(-1, 1)]
+        [InlineData(2, -9)]
+        [InlineData(0, 1)]
+        [InlineData(2,0)]
+        [InlineData(3, 20)]
+        [InlineData(21, 2)]
+        public void ReturnErrorWhenParamsAreNotCorrectOnCreated(int x, int y)
+        {
+            var location = Location.Create(x, y);
+            location.IsSuccess.Should().BeFalse();
+            location.Error.Should().NotBeNull();           
+        }
+
+        [Fact]
+        public void  CanCareteRandomLocation()
+        {
+            Location location = Location.CreateRandom();
+            location.Should().NotBeNull();
+            location.X.Should().BeGreaterThanOrEqualTo(1).And.BeLessThanOrEqualTo(10);
+            location.Y.Should().BeGreaterThanOrEqualTo(1).And.BeLessThanOrEqualTo(10);
+        }
+
+        [Fact]
+        public void BeNotEqualWhenAllPropertiesIsEqual()
+        {
+            Location firstLocation = Location.Create(2, 2).Value;
+            Location secondLocation = Location.Create(2, 2).Value;
+            (firstLocation == secondLocation).Should().BeTrue();
+        }
+
+        [Fact]
+        public void BeNotEqualWhenOneOfPropertiesIsNotEqual()
+        {
+            Location firstLocation = Location.Create(2, 2).Value;
+            Location secondLocation = Location.Create(2, 10).Value;
+            (firstLocation != secondLocation).Should().BeFalse();
+        }
+        public static IEnumerable<object[]> GetFakeLocations()
+        {
+            yield return [Location.Create(1, 1).Value, 0];
+            yield return [Location.Create(1, 2).Value, 1];
+            yield return [Location.Create(3, 1).Value, 2];
+            yield return [Location.Create(10, 10).Value, 18];
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFakeLocations))]
+        public void ReturnDistanceBetweenTwoLocations(Location anotherLocation, int distance)
+        {
+            Location location = Location.Create(1, 1).Value;       
+            var result = location.DistanceTo(anotherLocation);
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Should().Be(distance);
+        }
+
+    }
+}
